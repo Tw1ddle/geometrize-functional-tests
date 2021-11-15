c0c3f4b3817d57b6022cacbef629455d4e6de3a2
sam <samcodesit@gmail.com>
2021-11-15 02:35:34 +0000

    Fix up windows unattended install script, add an uninstall script to remove any existing installation too

--- get_geometrize_and_extract_to_app_subfolder.py
+++ get_geometrize_and_extract_to_app_subfolder.py
@@ -1,5 +1,5 @@
 # This script downloads the latest Geometrize installer/binaries for each platform
-# It then installs/unpacks the downloads to the /app subfolder
+# It then installs/unpacks the downloads to the /app subfolder in the repository
 # Note that to do this on Windows, it runs the Windows installer for Geometrize in unattended mode
 
 import os
@@ -14,6 +14,8 @@ import urllib.parse
 import urllib.request
 
 socket.setdefaulttimeout(600) # Downloading shouldn't take this long, idea is to cause a timeout
+download_bucket_url = 'https://geometrize-installer-bucket.s3.amazonaws.com/' # Where to get the installers from
+default_windows_breadcrumb = "windows/msvc2019_64" # Default to MSVC builds for Windows downloads
 
 # Helper for preventing divide by zero in download progress callback
 def safe_divide(x, y):
@@ -34,7 +36,7 @@ def downloadProgressCb(count, block_size, total_size):
     sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" % (percent, progress_size / (1024 * 1024), speed, duration))
     sys.stdout.flush()
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
+repo_root_dir = os.path.dirname(os.path.realpath(__file__))
 
 system_name = platform.system()
 
@@ -50,14 +52,11 @@ elif system_name == "Darwin":
 elif system_name == "Linux":
     breadcrumb = "linux"
 
-# Default to MSVC builds for Windows
 if breadcrumb == "windows":
-    breadcrumb = "windows/msvc2019_64"
-
-bucket_url = 'https://geometrize-installer-bucket.s3.amazonaws.com/'
+    breadcrumb = default_windows_breadcrumb
 
 # URL for file containing the name of the latest binary, which we ought to download and test with
-latest_tag_url = bucket_url + breadcrumb + "/__latest"
+latest_tag_url = download_bucket_url + breadcrumb + "/__latest"
 
 print("Searching for file containing name of build to test with at: " + latest_tag_url)
 
@@ -67,28 +66,53 @@ binary_file_name = open(latest_url_file_name ,"r").read().strip()
 
 print("Latest file is named " + binary_file_name + " - will download to repo root")
 
-latest_binary_url = bucket_url + urllib.parse.quote_plus(breadcrumb + "/" + binary_file_name)
+latest_binary_url = download_bucket_url + urllib.parse.quote_plus(breadcrumb + "/" + binary_file_name)
 urllib.request.urlretrieve(latest_binary_url, binary_file_name, downloadProgressCb)
 
 print("\n\nFinished downloading!\n\n")
 
-source_path = script_dir + "/" + binary_file_name
-target_path = script_dir + "/app"
-
-# Remove and then recreate the /app destination folder, initially
-if os.path.exists(target_path):
-    shutil.rmtree(target_path)
-
-# Create the app folder
-if not os.path.exists(target_path):
-    os.makedirs(target_path)
+source_path = repo_root_dir + "/" + binary_file_name
+target_path = repo_root_dir + "/app"
 
 # Unpack/install binary to "app" folder, same place for all platforms for symmetry
 if system_name == "Windows":
+    # Try uninstalling an existing installation first (to avoid gumming up the add/remove programs list when running locally)
+    maintenance_tool_path = repo_root_dir + "/app/" + "maintenancetool.exe"
+    if os.path.isfile(maintenance_tool_path):
+        print("Trying to silently uninstall a pre-existing installation from the app/ subfolder")
+        unattended_uninstaller_script_path = repo_root_dir + "/windows_unattended_uninstaller_script.qs"
+        try:
+            subprocess.check_call([maintenance_tool_path, '--script', unattended_uninstaller_script_path])
+        except subprocess.CalledProcessError:
+            pass # Error from the called uninstaller
+        except OSError:
+            pass # Executable not found
+
+        print("Waiting a few seconds for filesystem to settle...\n\n")
+        time.sleep(3) # Delay to avoid any filesystem-related timing issues
+    else:
+        print("Looks like there is no pre-existing installation, will go straight to installing")
+
+    # Create the /app folder
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+
     print("Installing app to app folder in unattended mode")
-    unattended_installer_path = script_dir + "/windows_unattended_install_script.qs"
-    subprocess.check_call([source_path, '--script', unattended_installer_path])
+    unattended_installer_script_path = repo_root_dir + "/windows_unattended_installer_script.qs"
+    try:
+        subprocess.check_call([source_path, '--script', unattended_installer_script_path])
+    except subprocess.CalledProcessError:
+        pass # Error from the called installer
+    except OSError:
+        pass # Executable not found
 else:
+    # Remove and then recreate the /app destination folder
+    if os.path.exists(target_path):
+        shutil.rmtree(target_path)
+    # Create the /app folder
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+
     print("Copying app from " + source_path + " to " + target_path)
 
     if system_name == "Darwin":
@@ -96,4 +120,8 @@ else:
     elif system_name == "Linux":
         shutil.copyfile(source_path, target_path + "/Geometrize.AppImage")
 
+print("Waiting few seconds for filesystem to settle...\n\n")
+time.sleep(3) # Delay to avoid any filesystem-related timing issues
+
+print("Listing resulting app directory after binary placement/installer unpacking\n\n")
 print(os.listdir(target_path))
